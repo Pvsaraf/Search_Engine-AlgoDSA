0f81340b472fd488c706ccfc90cb6a79af254184
Parth <parthsaraf2001@gmail.com>
2022-05-26 20:35:43 +0530

    Checking with static arr

--- index.js
+++ index.js
@@ -20,76 +20,76 @@ app.get("/", (req, res) => {
 });
 
 app.get("/search", (req, res) => {
-  const query = req.query;
+  // const query = req.query;
 
-  const question = query.question;
+  // const question = query.question;
 
-  // Tf IDF algo
+  // // Tf IDF algo
 
-  const childPython = spawn("python", ["./GenerateTopQuestions.py", question]);
-  childPython.stdout.on("data", function (data) {
-    var data_str = data.toString();
-    var arr = data_str.split("'], ['");
+  // const childPython = spawn("python", ["./GenerateTopQuestions.py", question]);
+  // childPython.stdout.on("data", function (data) {
+  //   var data_str = data.toString();
+  //   var arr = data_str.split("'], ['");
 
-    // console.log(arr.length);
-    var l = arr.length;
+  //   // console.log(arr.length);
+  //   var l = arr.length;
 
-    arr[0] = arr[0].slice(3);
-    // console.log(arr[0]);
+  //   arr[0] = arr[0].slice(3);
+  //   // console.log(arr[0]);
 
-    arr[l - 1] = arr[l - 1].slice(0, -5);
-    // console.log(arr[l - 1]);
+  //   arr[l - 1] = arr[l - 1].slice(0, -5);
+  //   // console.log(arr[l - 1]);
 
-    var str_arr = [];
+  //   var str_arr = [];
 
-    for (let i = 0; i < l; i++) {
-      let arr_next_split = [];
-      arr_next_split = arr[i].split("', '");
-      str_arr.push(arr_next_split);
-    }
-    // console.log(str_arr);
-    // arr = []
+  //   for (let i = 0; i < l; i++) {
+  //     let arr_next_split = [];
+  //     arr_next_split = arr[i].split("', '");
+  //     str_arr.push(arr_next_split);
+  //   }
+  //   // console.log(str_arr);
+  //   // arr = []
 
-    // for(let i=0;i<length(data_str)
+  //   // for(let i=0;i<length(data_str)
 
-    res.json(str_arr);
-  });
+  //   res.json(str_arr);
+  // });
 
-  childPython.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-  });
+  // childPython.stderr.on("data", (data) => {
+  //   console.error(`stderr: ${data}`);
+  // });
 
   // List of top 5 questions
 
   // setTimeout(() => {
-  //   const arr = [
-  //     {
-  //       title: "ABC",
-  //       url: "https://google.com",
-  //       statement: "The sum of two elements.",
-  //     },
-  //     {
-  //       title: "ABC",
-  //       url: "https://google.com",
-  //       statement: "The sum of two elements.",
-  //     },
-  //     {
-  //       title: "ABC",
-  //       url: "https://google.com",
-  //       statement: "The sum of two elements.",
-  //     },
-  //     {
-  //       title: "ABC",
-  //       url: "https://google.com",
-  //       statement: "The sum of two elements.",
-  //     },
-  //     {
-  //       title: "ABC",
-  //       url: "https://google.com",
-  //       statement: "The sum of two elements.",
-  //     },
-  //   ];
-  //   res.json(arr);
+  const arr = [
+    {
+      title: "ABC",
+      url: "https://google.com",
+      statement: "The sum of two elements.",
+    },
+    {
+      title: "ABC",
+      url: "https://google.com",
+      statement: "The sum of two elements.",
+    },
+    {
+      title: "ABC",
+      url: "https://google.com",
+      statement: "The sum of two elements.",
+    },
+    {
+      title: "ABC",
+      url: "https://google.com",
+      statement: "The sum of two elements.",
+    },
+    {
+      title: "ABC",
+      url: "https://google.com",
+      statement: "The sum of two elements.",
+    },
+  ];
+  res.json(arr);
   // }, 2000);
 });
 
